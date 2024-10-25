66f4f159a1b3299a8bc7c0af3f21b708b2fcd746
Gimsy <128653637+SarahyCaicedo@users.noreply.github.com>
2024-10-25 12:49:22 -0500

    Remodelación de la pagina, usando la plantilla que usamos en la sesión 1

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../styles/Card.css";
+
+const Card = ({ title, description, imageUrl, buttonLabel }) => {
+    return (
+        <div className="card">
+            <img src={imageUrl} alt={title} className="card-image" />
+            <div className="card-content">
+                <h2>{title}</h2>
+                <p>{description}</p>
+                <button className="card-button">{buttonLabel}</button>
+            </div>
+        </div>
+    );
+};
+
+export default Card;
