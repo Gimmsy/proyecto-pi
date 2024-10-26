c6496d76decbd1c0a8b1dbf5be48ad7f0911c375
Aura Pasmin <aura.pasmin@gmail.com>
2024-10-25 19:30:37 -0500

    changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Card from "../components/Card";
-import "../styles/HomaPage.css";
+import "../styles/HomePage.css";
 
 const HomePage = () => {
     return (
@@ -14,19 +14,19 @@ const HomePage = () => {
                 <Card
                     title="Contaminación del agua"
                     description="Breve descripción o algo llamativo para llamar la atención"
-                    imageUrl="/assets/image/contaminacion.png"
+                    imageUrl="/assets/image/contaminacion-agua.jpg"
                     buttonLabel="Ver más"
                 />
                 <Card
                     title="Escasez del agua"
                     description="Breve descripción del tema"
-                    imageUrl="/assets/image/escasez.png"
+                    imageUrl="/assets/image/escasez-agua.jpg"
                     buttonLabel="Ver más"
                 />
                 <Card
                     title="Acidificación de los océanos"
                     description="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-                    imageUrl="/assets/image/acidificacion.png"
+                    imageUrl="/assets/image/acidificacion-oceanos.jpg"
                     buttonLabel="Ver más"
                 />
             </section>
