2ad25935f9c5919aa6d1f1f6763a376675ce0a6c
Aura Pasmin <aura.pasmin@gmail.com>
2024-11-29 18:19:56 -0500

    add link in the logo

--- src/components/Slidebar.jsx
+++ src/components/Slidebar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import AccountMenu from "../components/AccountMenu";
 import "../styles/Slidebar.css";
 
@@ -11,19 +12,18 @@ const Sidebar = () => {
 
   return (
     <div className={`sidebar ${isOpen ? "open" : ""}`}>
-        <button className="hamburger text-2xl bg-primary text-white rounded md:hidden p-2 mr-4" onClick={toggleMenu}>
-          ☰
-        </button>
-      <div className="sidebar-header flex items-center justify-between p-12">
+      <button className="hamburger text-2xl bg-primary text-white rounded md:hidden p-2 mr-4" onClick={toggleMenu}>
+        ☰
+      </button>
+      <Link to="/home" className="sidebar-header flex items-center justify-between p-12">
         <img src="/assets/image/logopintado.png" alt="Logo AquaViva" className="logo w-20 h-auto mr-4" />
         <h1 className="text-xl font-bold">AquaViva</h1>
-      </div>
+      </Link>
 
       <nav className={`sidebar-nav ${isOpen ? "show" : ""} flex flex-col md:flex-row p-4 md:p-0`}>
-        <a href="/home" className="sidebar-item text-blue-500 p-2 text-lg transition-colors duration-300 hover:text-blue-700 hover:underline">Inicio</a>
-        <a href="/resources" className="sidebar-item text-blue-500 p-2 text-lg transition-colors duration-300 hover:text-blue-700 hover:underline">Recursos Adicionales</a>
-        <a href="/info" className="sidebar-item text-blue-500 p-2 text-lg transition-colors duration-300 hover:text-blue-700 hover:underline">Información</a>
-        <a href="/trivia" className="sidebar-item text-blue-500 p-2 text-lg transition-colors duration-300 hover:text-blue-700 hover:underline">Trivia</a>
+        <Link to="/home" className="sidebar-item text-blue-500 p-2 text-lg transition-colors duration-300 hover:text-blue-700 hover:underline">Inicio</Link>
+        <Link to="/info" className="sidebar-item text-blue-500 p-2 text-lg transition-colors duration-300 hover:text-blue-700 hover:underline">Información</Link>
+        <Link to="/trivia" className="sidebar-item text-blue-500 p-2 text-lg transition-colors duration-300 hover:text-blue-700 hover:underline">Trivia</Link>
       </nav>
 
       <div className="account-menu mt-auto p-4 md:p-0">
