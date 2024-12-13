9892740a5c7911547463bd85f03cfc9c35e06a84
Aura Pasmin <aura.pasmin@gmail.com>
2024-12-13 13:50:34 -0500

    add changes

--- src/components/Slidebar.jsx
+++ src/components/Slidebar.jsx
@@ -11,23 +11,25 @@ const Sidebar = () => {
   };
 
   return (
-    <div className={`sidebar ${isOpen ? "open" : ""}`}>
+    <div className={`sidebar ${isOpen ? "open" : ""} flex flex-col md:flex-row items-center justify-between p-4`}>
       <button className="hamburger text-2xl bg-primary text-white rounded md:hidden p-2 mr-4" onClick={toggleMenu}>
         ☰
       </button>
-      <Link to="/home" className="sidebar-header flex items-center justify-between p-12">
+      <Link to="/home" className="sidebar-header flex items-center justify-between p-4 md:p-0">
         <img src="/assets/image/logopintado.png" alt="Logo AquaViva" className="logo w-20 h-auto mr-4" />
         <h1 className="text-xl font-bold">AquaViva</h1>
       </Link>
 
-      <nav className={`sidebar-nav ${isOpen ? "show" : ""} flex flex-col md:flex-row p-4 md:p-0`}>
-        <Link to="/home" className="sidebar-item text-blue-500 p-5 text-2xl transition-colors duration-300 hover:text-blue-700 hover:underline">Inicio</Link>
-        <Link to="/info" className="sidebar-item text-blue-500 p-5 text-2xl transition-colors duration-300 hover:text-blue-700 hover:underline">Información</Link>
-        <Link to="/trivia" className="sidebar-item text-blue-500 p-5 text-2xl transition-colors duration-300 hover:text-blue-700 hover:underline">Desafío</Link>
-      </nav>
+      <div className="flex items-center">
+        <nav className={`sidebar-nav ${isOpen ? "show" : ""} flex flex-col md:flex-row p-4 md:p-0`}>
+          <Link to="/home" className="sidebar-item text-blue-500 p-5 text-2xl transition-colors duration-300 hover:text-blue-700 hover:underline">Inicio</Link>
+          <Link to="/info" className="sidebar-item text-blue-500 p-5 text-2xl transition-colors duration-300 hover:text-blue-700 hover:underline">Información</Link>
+          <Link to="/trivia" className="sidebar-item text-blue-500 p-5 text-2xl transition-colors duration-300 hover:text-blue-700 hover:underline">Desafío</Link>
+        </nav>
 
-      <div className="account-menu p-4 md:p-0 mr-8">
-        <AccountMenu />
+        <div className="account-menu p-4 ml-4">
+          <AccountMenu />
+        </div>
       </div>
     </div>
   );
